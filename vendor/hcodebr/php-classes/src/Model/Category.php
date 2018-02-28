2ddca4e9ec1efd523b4406f4288d4c93e717f406
Ângelo Hideki Noda <angelonoda@gmail.com>
2018-02-28 20:56:58 -0300

    Site - Categorias

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -28,6 +28,8 @@ class Category extends Model{
 		));
 
 		$this->setData($results[0]);
+
+		Category::updateFile();
 	}
 
 	public function get($idcategory)
@@ -42,25 +44,6 @@ class Category extends Model{
 		$this->setData($results[0]);
 	}
 
-	public function update()
-	{
-
-		$sql = new Sql();
-
-		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :indadmin)", array(
-			":iduser"=>$this->getiduser(),
-			":desperson"=>$this->getdesperson(),
-			":deslogin"=>$this->getdeslogin(),
-			":despassword"=>$this->getdespassword(),
-			":desemail"=>$this->getdesemail(),
-			":nrphone"=>$this->getnrphone(),
-			":indadmin"=>$this->getinadmin()
-		));
-
-		$this->setData($results[0]);
-			
-	}
-
 	public function delete()
 	{
 		$sql = new Sql();
@@ -68,9 +51,23 @@ class Category extends Model{
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory" , array(
 			":idcategory"=>$this->getidcategory()
 		));
+
+		Category::updateFile();
 	}
 
-	
+	public function updateFile()
+	{
+
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach ($categories as $row) {
+			array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
+	}	
 
 }
 
