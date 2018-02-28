c7c3ac48bdd392be8caa36926d05b8233855ee8e
Ângelo Hideki Noda <angelonoda@gmail.com>
2018-02-27 22:09:24 -0300

    CRUD categories

---
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class Category extends Model{
+
+	
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
+
+	}
+
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_categories_save(:idcategory, :descategory)", array(
+			":idcategory"=>$this->getidcategory(),
+			":descategory"=>$this->getdescategory()
+		));
+
+		$this->setData($results[0]);
+	}
+
+	public function get($idcategory)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+			':idcategory'=>$idcategory
+		]);
+
+		$this->setData($results[0]);
+	}
+
+	public function update()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :indadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":indadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+			
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory" , array(
+			":idcategory"=>$this->getidcategory()
+		));
+	}
+
+	
+
+}
+
+?>
