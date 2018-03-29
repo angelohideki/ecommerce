6ba2eab6d881895107bd3f66f6e417ceab4f1836
Ângelo Hideki Noda <angelonoda@gmail.com>
2018-03-28 22:11:50 -0300

    Categorias vs Produtos

--- views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/categories-menu.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,1 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Apple</a></li><li><a href="/categories/4">Google </a></li><li><a href="/categories/5">Motorola</a></li><li><a href="/categories/6">Samsung</a></li>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/7">Apple</a></li><li><a href="/categories/4">Google </a></li><li><a href="/categories/5">Motorola</a></li><li><a href="/categories/3">Samsung</a></li><li><a href="/categories/8">Sony</a></li>
